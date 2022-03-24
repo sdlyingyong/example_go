b79de2847b36e57fd9b1579dd08da4751e066aac
sdlyingyong <sdlyingyong@163.com>
2022-03-24 17:51:05 +0800

    极光推送

--- string/main.go
+++ string/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"regexp"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/httplib"
 )
 
 //demo 为案例
@@ -12,6 +17,9 @@ func main() {
 
 	//强密码检查
 	demoCheckStrongPwd()
+
+	//极光推送
+	demoJPush()
 
 }
 
@@ -28,6 +36,17 @@ func demoCheckStrongPwd() {
 		return
 	}
 	fmt.Println("demoCheckStrongPwd success")
+}
+
+func demoJPush() {
+	pushTags := make([]string, 0)
+	pushTags = append(pushTags, "id-xxxx") //设备号
+	ret, err := PushExec(pushTags, nil, "反馈处理", "您的反馈已经处理，请查看！", 2, "")
+	if err != nil {
+		fmt.Println("demoJPush failed, err: ", err)
+		return
+	}
+	fmt.Println("demoJPush success, ret: ", ret)
 }
 
 //检测强密码
@@ -54,4 +73,86 @@ func CheckStrongPwdExec(pwd string) error {
 		return errors.New("密码必须包含特殊符号")
 	}
 	return nil
+}
+
+//推送消息
+//@param tags 推送用户登录的token（用户登录被挤掉）没有可为nil 不能和alias同时为空
+//@param alias 推送用户所在组织的ID 没有可为nil 不能和tags同时为空
+//@param $type 消息类型 0=IOS APP版本更新 1=安卓版本更新 2=反馈回复 其他待定
+//@param msgId 消息ID
+type M map[string]interface{} //自定义map
+func PushExec(tags []string, alias []string, title, alert string, _type uint8, msgId string) (ret interface{}, err error) {
+
+	//准备极光的接口认证参数
+	req := httplib.Post("https://api.jpush.cn/v3/push")
+	req.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+	req.Header("Content-Type", "application/json")
+	appkey := ""                                                 //极光推送给你提供的appkey
+	secret := ""                                                 //极光推送给你提供的的secret
+	isProduct := beego.AppConfig.DefaultBool("isProduct", false) //是否产品模式
+	req.SetBasicAuth(appkey, secret)
+
+	//推送的对象设置
+	var param = M{}
+	param["platform"] = "all"
+	audience := M{}
+	if tags != nil {
+		audience["tag"] = tags
+	}
+	if alias != nil {
+		audience["alias"] = alias
+	}
+	param["audience"] = audience
+	var content = make(M)
+	if _type == 0 {
+		content["ios"] = M{
+			"alert":             M{"title": title, "body": alert},
+			"sound":             "default",
+			"badge":             "+1",
+			"extras":            M{"msgId": msgId, "type": _type},
+			"content-available": true,
+		}
+	} else if _type == 1 {
+		content["android"] = M{
+			"alert":      alert,
+			"title":      title,
+			"builder_id": 1,
+			"extras":     M{"msgId": msgId, "type": _type},
+		}
+	} else {
+		content["ios"] = M{
+			"alert":             M{"title": title, "body": alert},
+			"sound":             "default",
+			"badge":             "+1",
+			"extras":            M{"msgId": msgId, "type": _type},
+			"content-available": true,
+		}
+		content["android"] = M{
+			"alert":      alert,
+			"title":      title,
+			"builder_id": 1,
+			"extras":     M{"msgId": msgId, "type": _type},
+		}
+	}
+	param["notification"] = content
+	param["message"] = M{
+		"msg_content":  alert,
+		"content_type": "text",
+		"title":        title,
+		"extras":       M{"msgId": msgId, "type": _type}}
+	param["options"] = M{"apns_production": isProduct, "time_to_live": 60}
+
+	//发送请求并返回结果
+	a, _ := json.Marshal(param)
+	req.Body(string(a))
+	data, err := req.Bytes()
+	if err != nil {
+		return
+	}
+	ret = struct {
+		Sendno string
+		Msg_id string
+	}{}
+	json.Unmarshal(data, &ret)
+	return
 }
