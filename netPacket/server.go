b540b577047d5c274cbb0d35d9a21227bd0ff480
sdlyingyong <sdlyingyong@163.com>
2022-05-17 16:00:40 +0800

    增加tcp数据包的信息输出

--- netPacket/server.go
+++ netPacket/server.go
@@ -86,9 +86,57 @@ func showNetPacket() {
 	//读取包
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
+		//输出ip报文信息
 		//printIPPacket(packet)
-		printUDPInfo(packet)
+		//输出UDP报文信息
+		//printUDPInfo(packet)
+		//输出TCP报文信息
+		printTCPInfo(packet)
 	}
+}
+
+//输出TCP报文信息
+func printTCPInfo(packet gopacket.Packet) (err error) {
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer == nil {
+		return
+	}
+	tcp, ok := tcpLayer.(*layers.TCP)
+	if !ok {
+		err = errors.New("not tcp format")
+		return
+	}
+	if packet.ErrorLayer() != nil {
+		fmt.Println("Error decoding some part of the packet:", err)
+		return
+	}
+	//打印报文信息
+	//TCP header
+	//1.16位来源端口 16位目的地端口
+	//2.序号
+	//3.确认号
+	//4.4位数据偏移 6位保留字段 6位TCP标记 窗口大小
+	//5.校验和 紧急指针
+	fmt.Printf("src_port: %s \n", tcp.SrcPort.String())
+	fmt.Printf("dst_port: %s \n", tcp.DstPort.String())
+	fmt.Printf("seq_num: %d \n", tcp.Seq)
+	fmt.Printf("ack_num: %d \n", tcp.Ack)
+	fmt.Printf("data_offset: %d \n", tcp.DataOffset)
+	fmt.Printf("FIN: %t \n", tcp.FIN)
+	fmt.Printf("SYN: %t \n", tcp.SYN)
+	fmt.Printf("RST: %t \n", tcp.RST)
+	fmt.Printf("PSH: %t \n", tcp.PSH)
+	fmt.Printf("ACK: %t \n", tcp.ACK)
+	fmt.Printf("URG: %t \n", tcp.URG)
+	fmt.Printf("ECE: %t \n", tcp.ECE)
+	fmt.Printf("CWR: %t \n", tcp.CWR)
+	fmt.Printf("NS: %t \n", tcp.NS)
+	fmt.Printf("win_size: %d \n", tcp.Window)
+	fmt.Printf("checksum: %d \n", tcp.Checksum)
+	fmt.Printf("urg_point: %d \n", tcp.Urgent)
+	fmt.Println()
+
+	return
 }
 
 //输出UDP报文的信息
@@ -107,22 +155,21 @@ func printUDPInfo(packet gopacket.Packet) (err error) {
 		fmt.Println("Error decoding some part of the packet:", err)
 		return
 	}
-	//打印
+	//打印报文信息
+	//UDP报文格式
+	//1.16位来源端口号 16位目标端口号
+	//2.16位UDP长度 16位UDP校验和
+	//UDP数据...
 	fmt.Printf("src_port: %s \n", udp.SrcPort)
 	fmt.Printf("dst_port: %s \n", udp.DstPort)
 	fmt.Printf("udp_length: %d \n", udp.Length)
 	fmt.Printf("udp_checksum: %d \n", udp.Checksum)
 	fmt.Println()
-	return
 
+	return
 }
 
 //输出ip报文的信息
-// IP layer variables:
-// Version (Either 4 or 6)
-// IHL (IP Header Length in 32-bit words)
-// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
-// Checksum, SrcIP, DstIP
 func printIPPacket(packet gopacket.Packet) (err error) {
 	// Let's see if the packet is IP (even though the ether type told us)
 	ipLayer := packet.Layer(layers.LayerTypeIPv4)
@@ -142,6 +189,11 @@ func printIPPacket(packet gopacket.Packet) (err error) {
 	}
 
 	//打印
+	//IP报文格式
+	// Version (Either 4 or 6)
+	// IHL (IP Header Length in 32-bit words)
+	// TOS, Length, Id, Flags, FragOffset, TTL, Protocol (TCP?),
+	// Checksum, SrcIP, DstIP
 	fmt.Printf("%s to %s conn \n", ip.SrcIP.String(), ip.DstIP.String())
 	fmt.Println("ip_version", ip.Version)
 	fmt.Println("iph_length", ip.Length)
