13ac46ce2e4fd933d4842add3362a38fb31d6e4c
sdlyingyong <sdlyingyong@163.com>
2022-08-26 11:30:56 +0800

    add time count

--- air/main.go
+++ air/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 // //leetcode219
@@ -54,7 +55,33 @@ import (
 
 //程序入口
 func main() {
-	str := "_"
-	r := []rune(str)
-	fmt.Println(r)
+
+	//时间
+	now := time.Now()
+
+	//后续时间
+	// 	一、 复习点的确定（根据艾宾浩斯记忆曲线制定）：
+	// 　　1. 第一个记忆周期：5分钟
+	// 　　2. 第二个记忆周期：30分钟
+	// 　　3. 第三个记忆周期：12小时
+	// 　　4. 第四个记忆周期：1天
+	// 　　5. 第五个记忆周期：2天
+	// 　　6. 第六个记忆周期：4天
+	// 　　7. 第七个记忆周期：7天
+	// 　　8. 第八个记忆周期：15天
+	timeArr := []time.Duration{
+		time.Minute * 5,
+		time.Minute * 30,
+		time.Hour * 12,
+		time.Hour * 24,
+		time.Hour * 24 * 2,
+		time.Hour * 24 * 4,
+		time.Hour * 24 * 7,
+		time.Hour * 24 * 15,
+	}
+
+	for _, v := range timeArr {
+		fmt.Println(now.Add(v).Format("2006/01/02 15:04"))
+	}
+
 }
