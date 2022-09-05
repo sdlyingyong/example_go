2500fff767539183c9471bb8739cb7549bca9989
sdlyingyong <sdlyingyong@163.com>
2022-09-05 16:04:34 +0800

    add 周末不安排任务

--- air/main.go
+++ air/main.go
@@ -53,23 +53,40 @@ import (
 // 	return
 // }
 
+//检查是否是周末
+//	Sunday Weekday = iota
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
+// Saturday
+func checkWeekend(day time.Time) (isWeekend bool) {
+	if day.Weekday() >= 1 && day.Weekday() <= 5 {
+		isWeekend = false
+		return
+	}
+	isWeekend = true
+	return true
+}
+
 //程序入口
 func main() {
 
-	//时间
-	now := time.Now()
-
+	//当前时间
+	now := time.Now().Add(time.Hour * 24 * 5)
 	//后续时间
-	// 	一、 复习点的确定（根据艾宾浩斯记忆曲线制定）：
-	// 　　1. 第一个记忆周期：5分钟
-	// 　　2. 第二个记忆周期：30分钟
-	// 　　3. 第三个记忆周期：12小时
-	// 　　4. 第四个记忆周期：1天
-	// 　　5. 第五个记忆周期：2天
-	// 　　6. 第六个记忆周期：4天
-	// 　　7. 第七个记忆周期：7天
-	// 　　8. 第八个记忆周期：15天
 	timeArr := []time.Duration{
+		// 	一、 复习点的确定（根据艾宾浩斯记忆曲线制定）：
+		// 　　1. 第一个记忆周期：5分钟
+		// 　　2. 第二个记忆周期：30分钟
+		// 　　3. 第三个记忆周期：12小时
+		// 　　4. 第四个记忆周期：1天
+		// 　　5. 第五个记忆周期：2天
+		// 　　6. 第六个记忆周期：4天
+		// 　　7. 第七个记忆周期：7天
+		// 　　8. 第八个记忆周期：15天
+		time.Minute * 0,
 		time.Minute * 5,
 		time.Minute * 30,
 		time.Hour * 12,
@@ -80,8 +97,18 @@ func main() {
 		time.Hour * 24 * 15,
 	}
 
+	//周末不排任务
+	isWeekend := false
 	for _, v := range timeArr {
-		fmt.Println(now.Add(v).Format("2006/01/02 15:04"))
+		target := now.Add(v)
+		if isWeekend == false {
+			isWeekend = checkWeekend(target)
+		}
+		if isWeekend == true {
+			fmt.Println(target.Add(time.Hour * 24 * 2).Format("2006/01/02 15:04"))
+		} else {
+			fmt.Println(target.Format("2006/01/02 15:04"))
+		}
 	}
 
 }
